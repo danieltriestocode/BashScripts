ec751a638badbba6a3ae5beca3f7b3712e0ea757
Daniel <danielsemailforcodingstuff@yahoo.com>
2022-11-07 15:49:13 -0800

    Allow user to remove/replace a pattern in a filename

--- remove.sh
+++ remove.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-# Removes a keyword in a file name
+# Description: Removes spaces in filenames in current directory
+#              Replaces/Removes a specific pattern
+# Note:        If user wants to remove a pattern only then enter 0
+#              for the second arguement
 
-# for file in *; do mv "$file" `echo $file | tr ' ' '_'` ; done
+for file in *; do
+    mv "$file" > /dev/null 2>&1 `echo $file | tr ' ' '_'`
+done
 
-if [ -z $1 ] && [ -z $2 ]; then
-    echo "Please enter \"$0 <pattern> <new_pattern>\""
-else
-    for f in *$1*; do
-        mv -- "$f" "${f/$1/$2}"
-    done
+if ! [[ -z $2 ]]; then
+    if [ $2 == 0 ]; then
+        for f in *$1*; do
+            mv -- "$f" "${f/$1/}"
+        done
+    else
+        for f in *$1*; do
+            mv -- "$f" "${f/$1/$2}"
+        done
+    fi
 fi
