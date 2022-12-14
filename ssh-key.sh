14efe63c17c72768aef434788d3a8c4b99405b75
kfsdsd <gslgj>
2022-12-14 07:58:39 -0800

    Simplified ssh-key script

--- ssh-key.sh
+++ ssh-key.sh
@@ -1,45 +1,30 @@
 #!/bin/bash
 
-# This will create a new SSH key for your Github account
-# Note: This will put the keys in default location
-# Pass in your email address for the arguement
+# Description: This will create a new SSH key for your Github account
+# Note:        This will put the keys in default location (Home Directory)
 
-key_loc="$HOME/.ssh/id_ed25519"
-
-function _set_git_userEmail()
+function _git_config()
 {
-    read -p "Enter global username for Git: " name
-
     git config --global user.name "$name"
     git config --global user.email "$email"
+
+    printf "\nHere's your git config\n"
+    cat "$HOME/.gitconfig"
 }
 
 function _create_ssh-key()
 {
-    email=$1
-
     ssh-keygen -t ed25519 -C "$email"
     eval "$(ssh-agent -s)"
-    ssh-add $key_loc
 
-    isNotInstalled=$(dpkg-query -l xclip &> /dev/null | grep "no packages found" | wc -l)
-
-    if [ $isNotInstalled == 1 ]; then
-        printf "Copy the contents below and add it to your Github account\n\n"
-        cat "$key_loc.pub"
-    else
-        echo "Copied contents of SSH public key to clipboard"
-        xclip -sel clipboard -i < "$key_loc.pub"
-    fi
-
-
-    read -p "Would you like to configure global username and email address? (Y|y): " answer
-
-    if [ $answer == y ]; then _set_git_userEmail; fi
+    printf "\nCopy the contents below and add it to your Github account\n"
+    cat "$HOME/.ssh/id_ed25519.pub"
 }
 
-if [ -z $1 ]; then
-    echo "Please pass in your email address"
-else
-    _create_ssh-key $1
-fi
+printf "\nThis script will create an ssh-key for Git into your home directory\n\n"
+
+read -p "Please enter your name: " name
+read -p "Please enter your email address: " email
+
+_create_ssh-key
+_git_config
